471ade9bff1ff784b957c7da81f78a140c7a87d5
RiccTrev <trevisan.ricc@gmail.com>
2024-07-13 17:30:39 +0200

    Modificate estensioni accettate

--- app/config.py
+++ app/config.py
@@ -4,6 +4,6 @@ class Config:
     SECRET_KEY = 'your-secret-key'
     UPLOAD_FOLDER = os.path.join(os.path.expanduser('~'), 'FlaskSim', 'uploads')
     RESULT_FOLDER = os.path.join(os.path.expanduser('~'), 'FlaskSim', 'results')
-    ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+    ALLOWED_EXTENSIONS = {'xlsx'}
     SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
